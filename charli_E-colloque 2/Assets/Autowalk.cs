7b52112478797f5861c84743a1030b6b70764eaf
XavierMartel-Lachance <xavier@iMac-de-Xavier.local>
2016-11-21 09:49:00 -0500

    Animation_01

---
+++ charli_E-colloque 2/Assets/Autowalk.cs
@@ -0,0 +1,80 @@
+﻿
+using System;
+using UnityEngine;
+
+namespace VRStandardAssets.Utils
+{
+
+    public class Autowalk : MonoBehaviour
+    {
+
+        public float speed = 6.0f;
+
+
+        public bool teleport;
+
+        private SF_Spaceship Cockpit Transform;
+        private Transform MainCamera;
+
+        [SerializeField]
+        private VRInput m_InteractiveItem;
+
+
+        private void Awake()
+        {
+
+
+            controller = GetComponent<CharacterController>();
+            MainCamera = Camera.main.transform;
+
+
+        }
+
+
+
+
+
+
+
+
+        private void OnEnable()
+        {
+
+            m_InteractiveItem.OnCancel += moveUp;
+          
+        }
+
+        private void OnDisable()
+        {
+
+            m_InteractiveItem.OnCancel -= moveUp;
+           
+        }
+        void moveUp()
+        {
+            teleport = !teleport;
+            //Vector3 up = MainCamera.TransformDirection (Vector3.up);
+            //controller.SimpleMove (up * speed);
+
+
+
+        }
+
+  
+
+        void Update()
+        {
+            if (teleport == true)
+            {
+                Vector3 forward = MainCamera.TransformDirection(Vector3.forward);
+                controller.SimpleMove(forward * speed);
+
+            }
+
+        }
+
+
+    }
+
+
+}
